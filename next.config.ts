371f199e8486e0f5295aceff68c5c4396724996b
Rogério Chaves <rogeriochaves@users.noreply.github.com>
2025-11-15 17:13:15 +0100

    chore: add posthog

--- next.config.ts
+++ next.config.ts
@@ -25,6 +25,19 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  async rewrites() {
+    return [
+      {
+        source: "/ingest/static/:path*",
+        destination: "https://eu-assets.i.posthog.com/static/:path*",
+      },
+      {
+        source: "/ingest/:path*",
+        destination: "https://eu.i.posthog.com/:path*",
+      },
+    ];
+  },
+  skipTrailingSlashRedirect: true,
 };
 
 export default nextConfig;
